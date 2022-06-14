6454adac8df54fddd0b7bc2c8fa1e6f468b38673
LsCo <lennoxoo2007@gmail.com>
2022-06-14 23:11:53 +0200

    Update
    Update

--- private/script.js
+++ private/script.js
@@ -1,3 +1,19 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
 document.onload = test()
 function test(){
     if(getCookie("userid") === undefined || getCookie("userid") === ""){
